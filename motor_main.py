15ba275a1e7e0ff310bb436d0a562303bb94a737
matt <mtaylor@nuro.ai>
2021-04-19 13:51:22 -0700

    completed pwm

--- motor_main.py
+++ motor_main.py
@@ -8,11 +8,9 @@ import datetime
 import time
 import sys
 import random
-from gpiozero import LED
+import RPi.GPIO as GPIO
 
 ACTIVE_CHANNELS = 8
-
-motor_en = LED(22)
 
 x_len = 200
 y_range = [0, 3500]
@@ -20,12 +18,13 @@ y_range = [0, 3500]
 filename = str(datetime.datetime.now())
 file = open(filename, 'w', newline='')
 
+motor_en = 15
 pwmpin = 32             # PWM pin connected to LED
 GPIO.setwarnings(False)         #disable warnings
 GPIO.setmode(GPIO.BOARD)        #set pin numbering system
 GPIO.setup(pwmpin,GPIO.OUT)
-pi_pwm = GPIO.PWM(pwmpin,1000)      #create PWM instance with frequency
-pi_pwm.start(20)             #start PWM of required Duty Cycle 
+pi_pwm = GPIO.PWM(pwmpin,25000)      #create PWM instance with frequency
+GPIO.setup(motor_en, GPIO.OUT)
 
 plt.title('ADC Data')
 plt.xlabel('Time (ms)')
@@ -37,12 +36,12 @@ pwm_target = 0
 duration = 0
 pwm_current = 0
 
-so_file = "/home/pi/Documents/Motor Board/ad5592/ad5592_spi_read.so"
+so_file = "/home/pi/Documents/motor_board/ad5592/ad5592_spi_read.so"
 my_functions = CDLL(so_file)
 
 def initialize_spi():
     global initial_us
-    motor_en.on()
+    GPIO.output(motor_en, 1)
     if(my_functions.initialize_motor() == 0):
         print("Motor Initialize Successful!\n")
     else:
@@ -67,7 +66,7 @@ def get_us():
     
 def get_elapsed_us():
     temp = get_us()
-    return temp - initial_us
+    return (temp - initial_us)
 
 def find_position(code):
     if code == [1, 0, 1]:
@@ -93,50 +92,65 @@ def data_process(data):
     return adc_reading, index
 
 def user_inputs():
+    global duration
+    global pwm_current
+    global pwm_target
     duration = input("Enter sample duration (type 'r' for inifinite):")
     writer = csv.writer(file)
     writer.writerow(["Time (us)", "Signal 0", "Signal 1", "Signal 2", "Signal 3", "Signal 4", "Signal 5", "Signal 6", "Signal 7"])
     time.sleep(.1)
-    initial_us = get_us()
     pwm_target = input("Enter target duty cycle:")
     pwm_current = 0
     pi_pwm.start(pwm_current)
     
 def pwm_control():
-    if(pwm_current < pwm_target):
+    global pwm_current
+    if(pwm_current < int(pwm_target)):
         pwm_current = pwm_current + 1
     pi_pwm.ChangeDutyCycle(pwm_current)             #start PWM of required Duty Cycle
+    #print("PWM: {}".format(pwm_current))
 
-def pwm_rampdown():
+def motor_rampdown():
+    print("Starting rampdown...")
     for duty in range(pwm_current,-1,-1):
         pi_pwm.ChangeDutyCycle(duty)
-        sleep(0.2)
+        #print("PWM: {}".format(duty))
+        time.sleep(0.1)
+    GPIO.output(motor_en, 0)
     sys.exit()
     
 def read_adc():
+    global initial_us
     temp_data = np.uint32([0,0,0,0,0,0,0,0,0])
     adc_reading = 0x0
     index = 0x0
     dat_16bit = 0x0
+    pwm_counter = 0
+    initial_us = get_us()
     my_functions.getAnalogInAll_InitialSend()
     while(1):
-        pwm_control()
-        for i in range(0, ACTIVE_CHANNELS):
-            data_16bit = my_functions.getAnalogInAll_Receive()
-            #data = my_functions.getAnalogIn(i)
-            #print(data)
-            adc_reading, index = data_process(data_16bit)
-            #print('Data {}'.format(index) + ': {}'.format(adc_reading))
-            temp_data[index+1] = adc_reading
-        temp_data[0] = get_elapsed_us()
-        #print('Time Elapsed: {}'.format(temp_data[0]))
-        writer = csv.writer(file)
-        writer.writerow(temp_data)
-        if(duration != 'r'):
-            if(temp_data[0] >= int(duration) * 1000000):
-                my_functions.getAnalogInAll_Terminate()
-                pwm_rampdown()
-
+        try:
+            if((pwm_counter % 2) == 0):
+                pwm_control()
+            pwm_counter = pwm_counter + 1
+            for i in range(0, ACTIVE_CHANNELS):
+                data_16bit = my_functions.getAnalogInAll_Receive()
+                #data = my_functions.getAnalogIn(i)
+                #print(data)
+                adc_reading, index = data_process(data_16bit)
+                #print('Data {}'.format(index) + ': {}'.format(adc_reading))
+                temp_data[index+1] = adc_reading
+            temp_data[0] = get_elapsed_us()
+            print('Time Elapsed: {}'.format(temp_data[0]))
+            writer = csv.writer(file)
+            writer.writerow(temp_data)
+            if(duration != 'r'):
+                if(temp_data[0] >= int(duration) * 1000000):
+                    my_functions.getAnalogInAll_Terminate()
+                    motor_rampdown()
+        except KeyboardInterrupt:
+            my_functions.getAnalogInAll_Terminate()
+            motor_rampdown()
     
 if __name__ == "__main__":
     initialize_spi()
